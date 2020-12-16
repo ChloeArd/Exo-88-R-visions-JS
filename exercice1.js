161e9a8dfd061ad7a05afea42d232bced2073762
ChloeArd <chloe.ardoise@gmail.com>
2020-12-16 09:30:53 +0100

    Exercice 88

--- exercice1.js
+++ exercice1.js
@@ -1,22 +1,17 @@
 let board = [];
 
-
 document.getElementById("button1").addEventListener("click", function () {
     let value = document.getElementById("input1").value;
     board.push(value);
-    console.log(board);
 
     if (board.length === 10) {
         let div1 = document.createElement("div");
         let random = board[Math.floor(Math.random() * board.length)]
         div1.innerHTML = "aleatoire directement: " + random;
         document.getElementById("container").appendChild(div1);
-        console.log("chiffre aleatoire est " + random);
 
         let div2 = document.createElement("div");
-        console.log("La 5e valeur est " + board[4]);
         let size = board[4];
-        console.log("La 5e valeur est " + size);
         div2.innerHTML = "Le 5e mot est: " + size;
         document.getElementById("container").appendChild(div2);
     }
@@ -33,7 +28,6 @@ document.getElementById("aleatoire").addEventListener("click", function () {
     let random2 = board[Math.floor(Math.random() * board.length)];
     div4.innerHTML = "Aleatoire par l'utilisateur: " + random2;
     document.getElementById("container").appendChild(div4);
-    console.log("aleatoire par utilisateur: " + random2);
 })
 
 let div5 = document.createElement("div");
@@ -41,14 +35,12 @@ let div5 = document.createElement("div");
 document.getElementById("integrale").addEventListener("click", function () {
     let join = board.join(" - ");
     div5.innerHTML = join;
-    console.log("Bouton pour afficher les éléments du tableau" + board);
-    console.log("Bouton pour afficher les éléments du tableau " + join)
     document.getElementById("container").appendChild(div5);
 })
 //Bouton pour supprimer le dernier élément du tableau.
 document.getElementById("supprime1").addEventListener("click", function () {
     board.pop();
-    div5.innerHTML = board;
+    div5.innerHTML = board.join(" - ");
 })
 //Bouton pour supprimer tous les éléments du tableau.
 document.getElementById("supprime2").addEventListener("click", function () {
